e4fdee77cd5baf41ae7f9a41c23e4721bfa0c9f2
Aureo Gonzalez <aureogonz@hotmail.com>
2020-05-31 20:00:53 -0500

    Integro validador para formularios

---
+++ lib/services/validador_formuario.dart
@@ -0,0 +1,11 @@
+class ValidadorFormulario{
+  static String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Ingresa un email válido';
+    else
+      return null;
+  }
+}
